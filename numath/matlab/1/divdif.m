df1544babca0d5344c7d8fc4b822f3a57e81c7c0
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-31 11:38:40 +0100

    add horner

--- numath/matlab/1/divdif.m
+++ numath/matlab/1/divdif.m
@@ -3,17 +3,18 @@ function c = divdif(xdata, ydata)
         error('Length of input arguments must be equal.');
     end
 
-    c = [ydata(1)];
+    % Allocate space now for performance
+    c = [ydata(1), zeros(1, length(xdata)-1)];
     if length(xdata) == 1
         return;
     end
 
     for j = 2:length(xdata)
-        i=1
-        ipo = divdif(xdata(i+1:j),   ydata(i+1:j));
+        i = 1;
+        ipo = divdif(xdata(i+1:j), ydata(i+1:j));
         imo = divdif(xdata(i:j-1), ydata(i:j-1));
-        c = [c, (ipo(length(ipo)) -  imo(length(imo))) ...
-              ./ (xdata(j) - xdata(i))];
+        c(j) = (ipo(length(ipo)) -  imo(length(imo))) ...
+              ./ (xdata(j) - xdata(i));
     end
 
     return;
--- numath/matlab/1/horner.m
+++ numath/matlab/1/horner.m
@@ -1,7 +1,13 @@
-function [ output_args ] = horner( input_args )
-%HORNER Summary of this function goes here
-%   Detailed explanation goes here
+function [ yval ] = horner(c, xdata, xval)
+    if length(xdata) ~= length(c)
+        error('Length of xdata and c arguments must be equal.');
+    end
+    
+    % Allocate space now for performance
+    yval = zeros(length(xval));
 
-
+    for i = length(xdata):-1:1
+        yval = yval .* (xval - xdata(i)) + c(i);
+    end
 end
 
