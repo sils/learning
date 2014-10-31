639eca056a9e030875bd74abf08608f73eb866dc
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-31 11:01:42 +0100

    tmp

--- numath/matlab/1/divdif.m
+++ numath/matlab/1/divdif.m
@@ -1,3 +1,19 @@
 function c = divdif(xdata, ydata)
-  % TODO
+    if length(xdata) ~= length(ydata)
+        c = 'failed';
+        return;
+    end
+
+    c = [ydata(1)];
+    if length(xdata) == 1
+        return;
+    end
+
+    
+    j = length(xdata)
+    i=1
+    c = [(divdif(xdata(i+1:j),   ydata(i+1:j)) ...
+          -  divdif(xdata(i:j-1), ydata(i:j-1))) ...
+          ./ (xdata(j) - xdata(i))];
+    return;
 end
---
+++ numath/matlab/1/horner.m
@@ -0,0 +1,7 @@
+function [ output_args ] = horner( input_args )
+%HORNER Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+
