ec27270e8c1f0d2953bd7f59cd593809d56fa21b
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-10-31 13:53:18 +0100

    finished

--- numath/matlab/1/e.m
+++ numath/matlab/1/e.m
@@ -2,9 +2,15 @@ xdata = 5:0.5:14;
 ydata = [1.35,1.22,1.18,1.26,1.30,1.35,1.48,1.57,1.41,1.41,1.44,1.25,...
     1.33,1.45,1.3,1.22,1.32,1.3,1.37];
 
-xprecise = 5:0.1:14;
+% Interpolate also one year in the future
+xprecise = 5:0.1:15;
+% From matlab documentation: If x and Y are vectors of the same size, the not-a-knot end conditions are used.
 yspline = spline(xdata, ydata, xprecise);
 hold on
+% Plot will automatically interpolate linearly
 plot(xdata, ydata, 'r')
 plot(xprecise, yspline)
 hold off
+
+% Obviously (from plot) the spline is not suited to extrapolate future
+% values as it continues to rise.
