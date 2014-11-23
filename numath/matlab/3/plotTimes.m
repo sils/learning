bda0d3e43a3ef7da24b18963beb76b6c8f4c3fcd
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-11-23 15:26:23 +0100

    plotTimes: Add x values for hopefully better plot

--- numath/matlab/3/plotTimes.m
+++ numath/matlab/3/plotTimes.m
@@ -1,37 +1,42 @@
 function [] = plotTimes(nmax)
     backslash_times = nan(nmax, 3);
     inverse_times   = nan(nmax, 3);
+    x_vals          = nan(nmax, 3);
 
     for d = 1:3
         n = 2;
-        while(n^d <= nmax)
+        nd = n^d;
+        while(nd <= nmax)
             tic
             backslash(d, n);
-            backslash_times(n^d, d) = toc;
+            backslash_times(n, d) = toc;
 
             tic
             solve_inverse(d, n);
-            inverse_times(n^d, d) = toc;
+            inverse_times(n, d) = toc;
+
+            x_vals(n, d) = nd;
 
             n = n+1;
+            nd = n^d;
         end
     end
 
     hold on;
-    plot(backslash_times(:,1), 'r');
-    plot(backslash_times(:,2), 'g');
-    plot(backslash_times(:,3), 'b');
+    plot(x_vals(:,1), backslash_times(:,1), 'r');
+    plot(x_vals(:,2), backslash_times(:,2), 'g');
+    plot(x_vals(:,3), backslash_times(:,3), 'b');
     legend('computation via backslash, d=1', 'computation via backslash, d=2', 'computation via backslash, d=3');
     xlabel('n^d');
     ylabel('time in seconds');
     hold off;
-    
+
     figure;
-    
+
     hold on;
-    plot(inverse_times(:,1), 'r');
-    plot(inverse_times(:,2), 'g');
-    plot(inverse_times(:,3), 'b');
+    plot(x_vals(:,1), inverse_times(:,1), 'r');
+    plot(x_vals(:,2), inverse_times(:,2), 'g');
+    plot(x_vals(:,3), inverse_times(:,3), 'b');
     legend('computation via inverse, d=1', 'computation via inverse, d=2', 'computation via inverse, d=3')
     xlabel('n^d');
     ylabel('time in seconds');
