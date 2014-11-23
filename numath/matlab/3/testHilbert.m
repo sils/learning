42376d36ed5468133a2178e7d105cd56c9add6b8
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-11-23 15:45:06 +0100

    add testHilbert

---
+++ numath/matlab/3/testHilbert.m
@@ -0,0 +1,25 @@
+function [] = testHilbert()
+    maxn = 10;
+    close all;
+
+    conditions = zeros(maxn, 3);
+
+    for n = 1:maxn
+        H = hilb(n);
+
+        conditions(n, 1) = cond(H, 2);    % euclidic
+        conditions(n, 2) = cond(H, -inf)  % negative infinity
+        conditions(n, 3) = cond(H, 'fro') % frobenius
+    end
+
+    semilogy(conditions(:, 1), 'r');
+    hold on;
+    semilogy(conditions(:, 2), 'g');
+    semilogy(conditions(:, 3), 'b');
+
+    grid on;
+    legend('Euclidic', 'Negative Infinity', 'Frobenius');
+    xlabel('n');
+    ylabel('Conditions');
+    hold off;
+end
