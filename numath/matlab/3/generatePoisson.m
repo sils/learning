1d842819ca5d1bf2d9d32757bf064790f21765d0
JonasAckermann <jonas.p.ackermann@gmail.com>
2014-11-23 14:28:16 +0100

    tasks 3.1a, 3.1b

--- numath/matlab/3/generatePoisson.m
+++ numath/matlab/3/generatePoisson.m
@@ -1,5 +1,19 @@
 function [P] = generatePoisson(d, n)
-  if !ismember(d, [1,2,3]) || n < 2
+  if ~ismember(d, [1,2,3]) || n < 2
     error('Input values are invalid.');
+  end
+  P = full(gallery('tridiag', n, -1, 2, -1));
+  
+  % kron is associative and distributive
+  if d > 1
+      I = eye(n);
+      IP = kron(I, P);
+      PI = kron(P, I);
+
+      P = IP + PI;
+
+      if d>2
+        P = kron(I, P) + kron(PI, I);
+      end
   end
 end
