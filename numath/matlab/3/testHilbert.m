e4bd360496fb13681d67c33efe74e3d5a94f0126
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2014-11-23 15:58:43 +0100

    add errors

--- numath/matlab/3/testHilbert.m
+++ numath/matlab/3/testHilbert.m
@@ -3,6 +3,7 @@ function [] = testHilbert()
     close all;
 
     conditions = zeros(maxn, 3);
+    errors     = zeros(maxn, 3);
 
     for n = 1:maxn
         H = hilb(n);
@@ -10,6 +11,14 @@ function [] = testHilbert()
         conditions(n, 1) = cond(H, 2);     % euclidic
         conditions(n, 2) = cond(H, inf);   % infinity
         conditions(n, 3) = cond(H, 'fro'); % frobenius
+
+        v = ones(n, 1);
+        b = H * v;
+        x = H \ b;
+
+        errors(n, 1) = norm(x-v,  2   ) / norm(v,  2   ) % euclidic
+        errors(n, 1) = norm(x-v,  inf ) / norm(v,  inf ) % infinity
+        errors(n, 1) = norm(x-v, 'fro') / norm(v, 'fro') % frobenius
     end
 
     semilogy(conditions(:, 1), 'r');
@@ -21,5 +30,18 @@ function [] = testHilbert()
     legend('Euclidic', 'Infinity', 'Frobenius');
     xlabel('n');
     ylabel('Conditions');
+    hold off;
+
+
+    figure;
+    semilogy(errors(:, 1), 'r');
+    hold on;
+    semilogy(errors(:, 2), 'g');
+    semilogy(errors(:, 3), 'b');
+
+    grid on;
+    legend('Euclidic', 'Infinity', 'Frobenius');
+    xlabel('n');
+    ylabel('Errors');
     hold off;
 end
