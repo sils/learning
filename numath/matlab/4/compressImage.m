5caeed2ac9d4e245e99021035237c37f34645359
JonasAckermann <jonas.p.ackermann@gmail.com>
2014-12-08 13:45:45 +0100

    Finished task 3

--- numath/matlab/4/compressImage.m
+++ numath/matlab/4/compressImage.m
@@ -1,12 +1,21 @@
-function [] = compressImage(k)
+function [overallSize] = compressImage(k)
    
-    A=imread('funky.jpg');
-    % size(A);
+    A=imread('beer.jpg');
     A_gray = rgb2gray(A); 
     A=A_gray;
     [U,S,V] = svd(double(A));
     Ak=U(:,1:k)*S(1:k,1:k)*V(:,1:k)';
     imshow(uint8(Ak));
-    size(Ak)
+    
+    %some crap to check usefulness
+    aSize = size(A);
+    uSize = size(U(:,1:k));
+    sSize = size(S(1:k,1:k));
+    vSize = size(V(:,1:k));
+    
+    overallSize = (uSize(1)*uSize(2))+(sSize(1)*sSize(2))+(vSize(1)*vSize(2))
+    originalSize = (aSize(1))*(aSize(2))
+    overallSize/originalSize
+    
 
 end
