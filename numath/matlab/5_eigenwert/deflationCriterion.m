4a55bcc6df95fd3f9ff10c43a36a5fad39fce3a6
JonasAckermann <jonas.p.ackermann@gmail.com>
2015-01-12 21:02:49 +0100

    Revert "a"
    This reverts commit d37a6eaeb05989d406e188be5f6b18401c5e4812.

--- numath/matlab/5_eigenwert/deflationCriterion.m
+++ numath/matlab/5_eigenwert/deflationCriterion.m
@@ -1,7 +1,7 @@
 function i = deflationCriterion(A)
 %number of significant digits
     i = 0;  
-    t = 16;
+    t = 10;
     epsilon = 10 ^ -t;
     for k = 1:size(A,1)-1
         if (abs(A(k+1,k)) <= (abs(A(k,k)) + abs(A(k+1,k+1))) * epsilon)
--- numath/matlab/5_eigenwert/qr_shift.m
+++ numath/matlab/5_eigenwert/qr_shift.m
@@ -1,45 +1,44 @@
 function [ lambda, i ] = qr_shift(A, m_max)
-  if nargin < 2
+  if nargin < 3
     m_max = 1000;
   end;
 
-  it = 0;
+  i = 0;
   n = size(A, 1);
+  identity = eye(n);
   lambda = zeros(n, 1);
-  A = hess(A);
-  while(n > 1 && it < m_max)
-     while (it < m_max)
-        k = deflationCriterion(A);
-        if k > 0
-            [lambda(1:k), i1] = qr_shift(A(1:k,1:k), m_max - it);
-            [lambda(k+1:n), i2] = qr_shift(A(k+1:n,k+1:n), m_max-it);
-            it = it + max(i1, i2);
-            if it >= m_max
-                i = m_max;
-                return;
-            else
-                i = it;
-                lambda = sort(lambda);
-                return;
-            end;
+  Ai = hess(A);
+  while(n > 1 && i < m_max)
+    k = deflationCriterion(A);
+    if k > 0
+        [lambda(1:k), i1] = qr_shift(A(1:k,1:k));
+        [lambda(k+1:n), i2] = qr_shift(A(k+1:n,k+1:n));
+        i = i + max(i1, i2);
+        if i >= m_max
+            i = m_max;
+            return;
         else
-            ki = A(n, n);
-            ki_id = ki*eye(n);
-            [Qi, Ri] = qr(A - ki_id);
-            A = Ri*Qi + ki_id;
-            it = it+1;
-            if abs(A(n, n-1)) <= (1E-8)
-              lambda(n) = A(n, n);
-              break;
-            end;
+            lambda = sort(lambda);
+            return;
+        end;
+    else
+        ki = Ai(n, n)^i;
+        if abs(Ai(n, n-1)) <= 10^-8
+          lambda(n) = Ai(n, n);
+          n = n-1;
+          Ai = Ai(1:n, 1:n);
+          identity = eye(n);
+          if n == 1
+            lambda(1) = A(1, 1);
+            break;
+          end;
         end;
     end;
-    n = n-1;
-    A = A(1:n, 1:n);
+
+    ki_id = ki*identity;
+    [Qi, Ri] = qr(Ai - ki_id);
+    Ai = Ri*Qi + ki_id;
   end;
-  if n == 1
-      lambda(1) = A(1, 1);
-      lambda = sort(lambda);
-  end;
-  i = it;
+
+  lambda = sort(lambda);
 end
