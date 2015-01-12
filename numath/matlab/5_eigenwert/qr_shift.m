0de34503d86cd03b015d84316b4affca70f315d0
JonasAckermann <jonas.p.ackermann@gmail.com>
2015-01-12 21:11:24 +0100

    alles wieder auf anfang

--- numath/matlab/5_eigenwert/qr_shift.m
+++ numath/matlab/5_eigenwert/qr_shift.m
@@ -1,46 +1,45 @@
 function [ lambda, i ] = qr_shift(A, m_max)
-  if nargin < 3
+  if nargin < 2
     m_max = 1000;
   end;
 
-  i = 0;
+  it = 0;
   n = size(A, 1);
-  identity = eye(n);
   lambda = zeros(n, 1);
-  Ai = hess(A);
-  while(n > 1 && i < m_max)
-    %ab hier neu
-    k = deflationCriterion(A);
-    if k > 0
-        [lambda(1:k), i1] = qr_shift(A(1:k,1:k));
-        [lambda(k+1:n), i2] = qr_shift(A(k+1:n,k+1:n));
-        i = i + max(i1, i2);
-        if i >= m_max
-            i = m_max;
-            return;
+  A = hess(A);
+  while(n > 1 && it < m_max)
+     while (it < m_max)
+        k = deflationCriterion(A);
+        if k > 0
+            [lambda(1:k), i1] = qr_shift(A(1:k,1:k), m_max - it);
+            [lambda(k+1:n), i2] = qr_shift(A(k+1:n,k+1:n), m_max-it);
+            it = it + max(i1, i2);
+            if it >= m_max
+                i = m_max;
+                return;
+            else
+                i = it;
+                lambda = sort(lambda);
+                return;
+            end;
         else
-            lambda = sort(lambda);
-            return;
-        end;
-    else
-        %bis hier neu
-        ki = Ai(n, n)^i;
-        if abs(Ai(n, n-1)) <= 10^-8
-          lambda(n) = Ai(n, n);
-          n = n-1;
-          Ai = Ai(1:n, 1:n);
-          identity = eye(n);
-          if n == 1
-            lambda(1) = A(1, 1);
-            break;
-          end;
+            ki = A(n, n);
+            ki_id = ki*eye(n);
+            [Qi, Ri] = qr(A - ki_id);
+            A = Ri*Qi + ki_id;
+            it = it+1;
+            if abs(A(n, n-1)) <= (1E-8)
+              lambda(n) = A(n, n);
+              break;
+            end;
         end;
     end;
-
-    ki_id = ki*identity;
-    [Qi, Ri] = qr(Ai - ki_id);
-    Ai = Ri*Qi + ki_id;
+    n = n-1;
+    A = A(1:n, 1:n);
   end;
-
-  lambda = sort(lambda);
+  if n == 1
+      lambda(1) = A(1, 1);
+      lambda = sort(lambda);
+  end;
+  i = it;
 end
