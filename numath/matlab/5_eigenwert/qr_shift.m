1a4bd4d9337096490502fe7d4d88cfe4d7c67b45
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-12 17:20:54 +0100

    t

--- numath/matlab/5_eigenwert/qr_shift.m
+++ numath/matlab/5_eigenwert/qr_shift.m
@@ -1,9 +1,8 @@
-function [ lambda, i ] = qr_shift(A, m_max)
+function [ lambda ] = qr_shift(A, m_max)
   if nargin < 3
     m_max = 1000;
   end;
 
-  A = hess(A);
   n = size(A, 1);
   identity = eye(n);
   lambda = zeros(n, 1);
