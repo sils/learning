da8c8a58d2780b2c94efac2975136370a6b33508
JonasAckermann <jonas.p.ackermann@gmail.com>
2015-01-12 19:59:23 +0100

    lots of crap

---
+++ numath/matlab/5_eigenwert/deflationCriterion.m
@@ -0,0 +1,12 @@
+function i = deflationCriterion(A)
+%number of significant digits
+    i = 0;  
+    t = 16;
+    epsilon = 10 ^ -t;
+    for k = 1:size(A,1)-1
+        if (abs(A(k+1,k)) <= (abs(A(k,k)) + abs(A(k+1,k+1))) * epsilon)
+            i = k;
+            return;
+        end;
+    end;
+end
--- numath/matlab/5_eigenwert/qr_shift.m
+++ numath/matlab/5_eigenwert/qr_shift.m
@@ -1,23 +1,40 @@
-function [ lambda ] = qr_shift(A, m_max)
+function [ lambda, i ] = qr_shift(A, m_max)
   if nargin < 3
     m_max = 1000;
   end;
 
+  i = 0;
   n = size(A, 1);
   identity = eye(n);
   lambda = zeros(n, 1);
-  Ai = A;
-  for i = 0:m_max
-    ki = Ai(n, n)^i;
-    if Ai(n, n-1) <= 10^-8
-      lambda(n) = Ai(n, n);
-      n = n-1;
-      Ai = Ai(1:n, 1:n);
-      identity = eye(n);
-      if n == 1
-        lambda(1) = A(1, 1);
-        break;
-      end;
+  Ai = hess(A);
+  while(n > 1 && i < m_max)
+    %ab hier neu
+    k = deflationCriterion(A);
+    if k > 0
+        [lambda(1:k), i1] = qr_shift(A(1:k,1:k));
+        [lambda(k+1:n), i2] = qr_shift(A(k+1:n,k+1:n));
+        i = i + max(i1, i2);
+        if i >= m_max
+            i = m_max;
+            return;
+        else
+            lambda = sort(lambda);
+            return;
+        end;
+    else
+        %bis hier neu
+        ki = Ai(n, n)^i;
+        if abs(Ai(n, n-1)) <= 10^-8
+          lambda(n) = Ai(n, n);
+          n = n-1;
+          Ai = Ai(1:n, 1:n);
+          identity = eye(n);
+          if n == 1
+            lambda(1) = A(1, 1);
+            break;
+          end;
+        end;
     end;
 
     ki_id = ki*identity;
