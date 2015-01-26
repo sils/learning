1532d81fa2a1ae2be990d7db7be9c591d23ff295
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-26 17:56:28 +0100

    bisec

--- numath/matlab/6/test.m
+++ numath/matlab/6/test.m
@@ -8,10 +8,19 @@ f = @(x, beta) a_1 / a_2 * cos(beta) - a_1 / a_4 * cos(x) - cos(beta - x) + (a_1
 df = @(x, beta) a_1 / a_4 * sin(x) + sin(x - beta);
 
 % From k directly
-f_k = @(x, k) f(x, beta_k(k));
-df_k = @(x, k) df(x, beta_k(k));
+f_50 = @(x) f(x, beta_k(50));
+df_50 = @(x) df(x, beta_k(50));
 
 tol = 1E-8;
 x = -10:0.1:10;
-y = f_k(x, 50);
+y = f_50(x);
 plot(x,y);
+
+% TEST 1 - "Bisektion"
+a = -6;
+b = -4;
+[alpha_50_1, ~, iter_1] = bisektion(f_50, a, b, tol)
+
+a = -2;
+b = 2;
+[alpha_50_2, ~, iter2] = bisektion(f_50, a, b, tol)
