89a173bd6cdc1ebf72aef7ca39c7b8995d0da89e
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-26 17:24:33 +0100

    bisektion: params optonal

--- numath/matlab/6/bisektion.m
+++ numath/matlab/6/bisektion.m
@@ -1,4 +1,11 @@
 function [zero, res, niter] = bisektion(f, a, b, tol, nmax)
+  if nargin < 5
+    if nargin < 4
+      tol = eps;
+    end
+    nmax = 1000;
+  end
+
   fa = f(a);
   fb = f(b);
   niter = 0;
