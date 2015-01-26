312e6ad9ddeeaf85ec480292d7f3725f356d67ee
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-26 17:51:05 +0100

    asdf

--- numath/matlab/6/test.m
+++ numath/matlab/6/test.m
@@ -3,7 +3,7 @@ a_2 = 13;
 a_3 = 8;
 a_4 = 10;
 beta_k = @(k) k * pi / 100;
-f = @(x,beta) (a_1./a_2).*cos(beta)-(a_1./a_4).*cos(x)-cos(beta-x)+(((a_1.^2)+(a_2.^2)+(a_3.^2)+(a_4.^2))./(2.*a_2.*a_4));
+f = @(x, beta) a_1 / a_2 * cos(beta) - a_1 / a_4 * cos(x) - cos(beta - x) + (a_1^2+a_2^2-a_3^2+a_4^2)/(2*a_2*a_4);
 % Derivation
 df = @(x, beta) a_1 / a_4 * sin(x) + sin(x - beta);
 
