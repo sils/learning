7ab2619d4143d5f3a61df4c573ab8524453ccedf
Lasse Schuirmann <lasse.schuirmann@gmail.com>
2015-01-26 17:21:45 +0100

    indentation

--- numath/matlab/6/bisektion.m
+++ numath/matlab/6/bisektion.m
@@ -1,19 +1,19 @@
 function [zero, res, niter] = bisektion(f, a, b, tol, nmax)
-    fa = f(a);
-    fb = f(b);
-    niter = 0;
-    while b - a > tol && niter < nmax
-        c = 0.5 * (a + b);
-        fc = f(c);
-        if fa * fc > 0
-            a = c;
-            fa = fc;
-        else
-            b = c;
-            fb = fc;
-        end
-        niter = niter + 1;
+  fa = f(a);
+  fb = f(b);
+  niter = 0;
+  while b - a > tol && niter < nmax
+    c = 0.5 * (a + b);
+    fc = f(c);
+    if fa * fc > 0
+      a = c;
+      fa = fc;
+    else
+      b = c;
+      fb = fc;
     end
-    zero = (a + b) / 2;
-    res = f(zero);
+    niter = niter + 1;
+  end
+  zero = (a + b) / 2;
+  res = f(zero);
 end
--- numath/matlab/6/newton.m
+++ numath/matlab/6/newton.m
@@ -1,14 +1,14 @@
 function [zero, res, niter] = newton(f, df, x0, tol, nmax)
-    x_alt = x0;
-    niter = 0;
-    while 1
-        x_neu = x_alt - f(x_alt) / df(x_alt);
-        niter = niter + 1;
-        if abs(x_neu - x_alt) < tol || niter >= nmax
-            break;
-        end
-        x_alt = x_neu;
+  x_alt = x0;
+  niter = 0;
+  while 1
+    x_neu = x_alt - f(x_alt) / df(x_alt);
+    niter = niter + 1;
+    if abs(x_neu - x_alt) < tol || niter >= nmax
+      break;
     end
-    zero = x_neu;
-    res = f(zero);
+    x_alt = x_neu;
+  end
+  zero = x_neu;
+  res = f(zero);
 end
